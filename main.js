0c34dfaf9897cfcf70c7235dafd42f7504cc4184
Bhargav R. Patel <ziikutv@gmail.com>
2015-07-06 19:27:07 -0400

    Finish asynchronous authentication functionality

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+"use strict"
+
 var
     app           = require('app'),
     ipc           = require('ipc'),   // Module to have interprocess communication
@@ -30,7 +32,65 @@ app.on('ready', function () {
     mainWindow.show();  // Show window
   });
 
+  ipc.on('auth-request', function(event, arg) {
+    authenticate(function (error, result) {  // Call the authentication method which will return the authentication code to us
+      if (error) {
+        console.error("User closed window and did not authenticate");
+      } else {
+        mainWindow.webContents.send('auth-result', 'Success with code = ' + result);  // Send 'Success' to render process
+      }
+    });
+  });
+
   mainWindow.on('closed', function () {
     mainWindow = null;  // Dereference window
   });
 });
+
+
+/* Gets authentication code */
+function authenticate (callback) {
+  // Use let instead of var for block scoped variables, this is why we had to add "use strict" at the top of the file
+  let client_id = "2060";
+  let callback_uri = "https://localhost/callback";
+  // Create url using ES6 template strings (yey!)
+  // let url = `https://put.io`;  // uncommnet and comment line below to logout for testing only
+  let url = `https://api.put.io/v2/oauth2/authenticate?client_id=${client_id}&response_type=code&redirect_uri=${callback_uri}`;
+  let code = null;
+
+  // Create authentication window, pointing to Put.io API auth page
+  let authWindow = new BrowserWindow({
+    width: 500,
+    height: 800,
+    show: false,      // hide window initially
+    // frame: false,
+    'node-integration': false
+  });
+
+  // Start loading Put.io's page
+  authWindow.loadUrl(url);
+
+  // Present window to the user once it has been fully loaded
+  authWindow.webContents.on('did-stop-loading', function () {
+    authWindow.show();
+  });
+
+  // Detect form completion event
+  authWindow.webContents.on('did-get-redirect-request', function (event, oldUrl, newUrl, isMainFrame) {
+    if (newUrl.startsWith(callback_uri)) {
+      // Strip code from the uri (Expected format: https://localhost/callback?code=COOKIES-HERE)
+      code = newUrl.split(/[=]/)[1];
+      authWindow.close(); // Close the auth window (This will trigger the close event, so we will clean up the references properly)
+    }
+  });
+
+  // Close window
+  authWindow.on('close', function() {
+    authWindow = null;
+    if (code) {
+      callback(null, code);
+    } else {
+      callback(new Error("Authentication failed, window closed"));
+    }
+  });
+}
