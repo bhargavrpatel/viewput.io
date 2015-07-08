ca0ea2be3beffc16e4f7d877733e0fc68aed2520
Bhargav R. Patel <ziikutv@gmail.com>
2015-07-08 16:24:50 -0400

    Add riotjs and gulp task to compile .tag files

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,10 +1,11 @@
 var
-    gulp    = require('gulp'),
-    babel   = require('gulp-babel'),
-    watch   = require('gulp-watch'),
-    uglify  = require('gulp-uglify'),
-    strip   = require('gulp-strip-comments'),
-    rename  = require('gulp-rename');
+    gulp    =   require('gulp'),
+    babel   =   require('gulp-babel'),
+    watch   =   require('gulp-watch'),
+    uglify  =   require('gulp-uglify'),
+    strip   =   require('gulp-strip-comments'),
+    rename  =   require('gulp-rename');
+    riot    =   require('riot');
 
 // Transpile ES6 app to ES5 using babel
 gulp.task('transpile-app', function () {
@@ -16,6 +17,7 @@ gulp.task('transpile-app', function () {
     .pipe(gulp.dest('app'))              // Save it in the same directory
 });
 
+
 // Transpile general ES6 javascripts
 gulp.task('transpile-scripts', function () {
   return gulp.src('browser/src/es6-js/**.es6.js')
@@ -23,10 +25,33 @@ gulp.task('transpile-scripts', function () {
     .pipe(babel())                       // Babel ES5 -> ES6
     .pipe(uglify())                      // uglify
     .pipe(rename(function (path) {       // Rename files so .es6.js -> .js
-      path.basename = path.basename.replace('.es6', '');
+      path.basename = path.basename.replace('es6', 'dist');
     }))
     .pipe(gulp.dest('browser/build/js'))
 });
+
+
+// Compile all of riot.js tag files
+gulp.task('riot', function() {
+  return gulp.src('browser/src/riot-tags/**.tag')
+    .pipe(compileTAGtoJS())           // Custom Compile function
+    .pipe(babel())                       // Babel ES5 -> ES6
+    .pipe(uglify())                      // uglify
+    .pipe(rename(function (path) {       // Rename files so .es6.js -> .js
+      console.log(path);
+      path.extname = '.tag.js';
+    }))
+    .pipe(gulp.dest('browser/build/js/riot-components'))
+});
+
+/* Converts Riotjs DSL to javascript */
+function compileTAGtoJS() {
+  function transform(file, callback) {
+    file.contents = new Buffer(riot.compile(String(file.contents)));
+    callback(null, file);
+  }
+  return require('event-stream').map(transform);
+}
 
 
 gulp.task('default', ['transpile-app'])
