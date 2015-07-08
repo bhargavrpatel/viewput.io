d3723725c6ed514eefaa89850d5f2c539f954da1
Bhargav R. Patel <ziikutv@gmail.com>
2015-07-08 15:41:52 -0400

    Add task to transpile all general es6 scripts

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,7 +2,7 @@ var
     gulp    = require('gulp'),
     babel   = require('gulp-babel'),
     watch   = require('gulp-watch'),
-    uglify   = require('gulp-uglify'),
+    uglify  = require('gulp-uglify'),
     strip   = require('gulp-strip-comments'),
     rename  = require('gulp-rename');
 
@@ -16,10 +16,17 @@ gulp.task('transpile', function () {
     .pipe(gulp.dest('app'))              // Save it in the same directory
 });
 
-// // Watch the main.es6.js file for changes and transpile automatically
-// gulp.task('watch', function () {
-//   watch('app/main.es6.js', ['transpile']);
-// })
+// Transpile general ES6 javascripts
+gulp.task('transpile-scripts', function () {
+  return gulp.src('browser/src/es6-js/**.es6.js')
+    .pipe(strip())                       // Strip comments
+    .pipe(babel())                       // Babel ES5 -> ES6
+    .pipe(uglify())                      // uglify
+    .pipe(rename(function (path) {       // Rename files so .es6.js -> .js
+      path.basename = path.basename.replace('.es6', '');
+    }))
+    .pipe(gulp.dest('browser/build/js'))
+});
 
 
 gulp.task('default', ['transpile'])
