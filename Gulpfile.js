429df23d0cefa4d1189da803d100708aa67503b9
Bhargav R. Patel <ziikutv@gmail.com>
2015-07-08 12:42:15 -0400

    Add JS ulgify to the transpile pipeline

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,6 +2,7 @@ var
     gulp    = require('gulp'),
     babel   = require('gulp-babel'),
     watch   = require('gulp-watch'),
+    uglify   = require('gulp-uglify'),
     strip   = require('gulp-strip-comments'),
     rename  = require('gulp-rename');
 
@@ -10,7 +11,8 @@ gulp.task('transpile', function () {
   return gulp.src('app/main.es6.js')
     .pipe(strip())                       // Strip comments
     .pipe(babel())                       // Pipe the file into babel
-    .pipe(rename('main.js'))             // rename to main.js
+    .pipe(uglify())                       // Pipe the file into babel
+    .pipe(rename('main.dist.js'))             // rename to main.js
     .pipe(gulp.dest('app'))              // Save it in the same directory
 });
 
